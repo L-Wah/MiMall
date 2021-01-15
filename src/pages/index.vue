128ac74d4f266ee5dac02de343d78e95a60199f4
user <875264338@qq.com>
2021-01-15 16:48:06 +0800

    swiper-box

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,14 +1,319 @@
 <template>
-  <div>
+  <div class="index">
+    <div class="container">
+      <div class="swiper-box">
+        <div class="nav-menu">
+          <ul class="menu-wrap">
+            <li class="menu-item">
+              <a href="javascript:;">手机 电话卡</a>
+              <div class="children">
+                <!-- 循环数组第一层 -->
+                <ul v-for="(item,i) in menuList" :key="i">
+                <!-- 循环数组第二层 -->
+                  <li v-for="(sub,j) in item" :key="j">
+                    <a :href="sub?'/#/product/'+sub.id:''">
+                      <img :src="sub?sub.img:'/imgs/item-box-1.png'" alt="">
+                      {{sub?sub.name:'小米9'}}
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </li>
+            <li class="menu-item">
+              <a href="javascript:;">电视 盒子</a>
+            </li>
+            <li class="menu-item">
+              <a href="javascript:;">笔记本 平板</a>
+            </li>
+            <li class="menu-item">
+              <a href="javascript:;">家电 插线板</a>
+            </li>
+            <li class="menu-item">
+              <a href="javascript:;">出行 穿戴</a>
+            </li>
+            <li class="menu-item">
+              <a href="javascript:;">智能 路由器</a>
+            </li>
+            <li class="menu-item">
+              <a href="javascript:;">电源 配件</a>
+            </li>
+            <li class="menu-item">
+              <a href="javascript:;">生活 箱包</a>
+            </li>
+          </ul>
+        </div>
+        <swiper :options="swiperOption">
+          <swiper-slide v-for="(item,index) in slideList" :key="item.id">
+           <a v-bind:href="'/#/product/'+item.id">
+              <img v-bind:src="item.img">
+            </a>
+          </swiper-slide>
+          <div class="swiper-pagination"  slot="pagination"></div>
+          <div class="swiper-button-prev" slot="button-prev"></div>
+          <div class="swiper-button-next" slot="button-next"></div>
+        </swiper>
+      </div>
+    </div>
     <service-bar></service-bar>
   </div>
 </template>
 <script>
   import ServiceBar from './../components/ServiceBar'
+  // import Modal from './../components/Modal'
+  import { swiper, swiperSlide } from 'vue-awesome-swiper'
+  import 'swiper/dist/css/swiper.css'
   export default {
   name:'index',
   components:{
-    ServiceBar
-  }
+    swiper,
+    swiperSlide,
+    ServiceBar,
+    // Modal
+  },
+  data() {
+    return {
+      swiperOption:{
+        autoplay:true,
+        loop:true,
+        effect:'cube',
+        // slide的切换效果，默认为"slide"（位移切换），可设置为'slide'（普通切换、默认）,"fade"（淡入）"cube"（方块）"coverflow"（3d流）"flip"（3d翻转）。
+        cubeEffect: {
+          shadowOffset: 100,
+          shadowScale: 0.6
+        },
+        pagination: {
+          el: '.swiper-pagination',
+          clickable:true
+        },
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        }
+      },
+      slideList:[
+        {
+          id:'42',
+          img:'/imgs/slider/slide-1.jpg'
+        },
+        {
+          id:'45',
+          img:'/imgs/slider/slide-2.jpg'
+        },
+        {
+          id:'46',
+          img:'/imgs/slider/slide-3.jpg'
+        },
+        {
+          id:'47',
+          img:'/imgs/slider/slide-4.jpg'
+        },
+        {
+          id:'48',
+          img:'/imgs/slider/slide-5.jpg'
+        }
+      ],
+      menuList:[
+        [
+          {
+            id:30,
+            img:'/imgs/item-box-1.png',
+            name:'小米CC9',
+          },{
+            id:31,
+            img:'/imgs/item-box-2.png',
+            name:'小米8青春版',
+          },{
+            id:32,
+            img:'/imgs/item-box-3.jpg',
+            name:'Redmi K20 Pro',
+          },{
+            id:33,
+            img:'/imgs/item-box-4.jpg',
+            name:'移动4G专区',
+          }
+        ],
+        [0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]
+      ],
+      phoneList:[],
+    }
+  },
 }
 </script>
+<style lang="scss" scope>
+  @import './../assets/scss/config.scss';
+  @import './../assets/scss/mixin.scss';
+  .index{
+    .swiper-box{
+      .nav-menu{
+        position:absolute;
+        width:264px;
+        height:451px;
+        z-index:9;
+        padding:26px 0;
+        background-color:#55585a7a;
+        box-sizing:border-box;
+        .menu-wrap{
+          .menu-item{
+            height:50px;
+            line-height:50px;
+            a{
+              position:relative;
+              display:block;
+              font-size:16px;
+              color:#ffffff;
+              padding-left:30px;
+              &:after{
+                position:absolute;
+                right:30px;
+                top:17.5px;
+                content:' ';
+                @include bgImg(10px,15px,'/imgs/icon-arrow.png');
+              }
+            }
+            &:hover{
+              background-color:$colorA;
+              .children{
+                display:block;
+              }
+            }
+            .children{
+              display:none;
+              width:962px;
+              height:451px;
+              background-color:$colorG;
+              position:absolute;
+              top:0;
+              left:264px;
+              border:1px solid $colorH;
+              ul{
+                display:flex;
+                justify-content:space-between;
+                height:75px;
+                li{
+                  height:75px;
+                  line-height:75px;
+                  flex:1;
+                  padding-left:23px;
+                }
+                a{
+                  color:$colorB;
+                  font-size:14px;
+                }
+                img{
+                  width:42px;
+                  height:35px;
+                  vertical-align:middle;
+                  margin-right:15px;
+                }
+              }
+            }
+          }
+        }
+      }
+      .swiper-container {
+        height: 451px;
+        .swiper-button-prev{
+          left:274px;
+        }
+        img{
+          width:100%;
+          height:100%;
+        }
+      }  
+    }
+    .ads-box{
+      @include flex();
+      margin-top:14px;
+      margin-bottom:31px;
+      a{
+        width:296px;
+        height:167px;
+      }
+    }
+    .banner{
+      margin-bottom:50px;
+    }
+    .product-box{
+      background-color:$colorJ;
+      padding:30px 0 50px;
+      h2{
+        font-size:$fontF;
+        height:21px;
+        line-height:21px;
+        color:$colorB;
+        margin-bottom:20px;
+      }
+      .wrapper{
+        display:flex;
+        .banner-left{
+          margin-right:16px;
+          img{
+            width:224px;
+            height:619px;
+          }
+        }
+        .list-box{
+          .list{
+            @include flex();
+            width:986px;
+            margin-bottom:14px;
+            &:last-child{
+              margin-bottom:0;
+            }
+            .item{
+              width:236px;
+              height:302px;
+              background-color:$colorG;
+              text-align:center;
+              span{
+                display:inline-block;
+                width:67px;
+                height:24px;
+                font-size:14px;
+                line-height:24px;
+                color:$colorG;
+                &.new-pro{
+                  background-color:#7ECF68;
+                }
+                &.kill-pro{
+                  background-color:#E82626;
+                }
+              }
+              .item-img{
+                img{
+                  width:100%;
+                  height:195px;
+                }
+              }
+              .item-info{
+                h3{
+                  font-size:$fontJ;
+                  color:$colorB;
+                  line-height:$fontJ;
+                  font-weight:bold;
+                }
+                p{
+                  color:$colorD;
+                  line-height:13px;
+                  margin:6px auto 13px;
+                }
+                .price{
+                  color:#F20A0A;
+                  font-size:$fontJ;
+                  font-weight:bold;
+                  cursor:pointer;
+                  &:after{
+                    @include bgImg(22px,22px,'/imgs/icon-cart-hover.png');
+                    content:' ';
+                    margin-left:5px;
+                    vertical-align: middle;
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+</style>
