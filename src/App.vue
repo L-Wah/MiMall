679827454636f9bac9a66effda9a8d81c1e17c06
user <875264338@qq.com>
2021-01-17 15:54:40 +0800

    login

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,30 @@
 <template>
   <div id="app">
-    <router-view/>
+    <router-view />
   </div>
 </template>
 <style>
-  @import './assets/scss/reset.scss';
+@import "./assets/scss/reset.scss";
 </style>
-// <script>
-// import storge from './storage/index'(index可省略)
-// import storge from './storage'
-// export default {
-//   name:'app',
-//   data() {
-//     return {   
-//     }
-//   },
-//   mounted(){
-    
-//   },
-// }
-// </script>
+<script>
+export default {
+  name: "app",
+  data() {
+    return {};
+  },
+  methods: {
+    getUser() {
+      this.axios.get("/user").then((res) => {});
+    },
+    getCartCount() {
+      this.axios.get("/carts/products/sum").then((res) => {});
+    },
+  },
+  mounted() {
+    if (this.$cookie.get("userId")) {
+      this.getUser();
+      this.getCartCount();
+    }
+  },
+};
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -38,6 +38,11 @@ const routes = [
     ]
   },
   {
+    path: '/login',
+    name: 'login',
+    component: () => import('../pages/login.vue')
+  },
+  {
     path:'/cart',
     name:'cart',
     component:Cart
