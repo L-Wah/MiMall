dd7d227308b8a7c1fdf4b4fc26719688cb597980
user <875264338@qq.com>
2021-01-22 20:47:07 +0800

    优化

--- src/router/index.js
+++ src/router/index.js
@@ -2,14 +2,6 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../pages/home'
 import Index from '../pages/index'
-import Product from '../pages/product'
-import Detail from '../pages/detail'
-import Cart from '../pages/cart'
-import Order from '../pages/order'
-import OrderConfirm from '../pages/orderConfirm'
-import OrderList from '../pages/orderList'
-import OrderPay from '../pages/orderPay'
-import AliPay from '../pages/alipay'
 
 Vue.use(VueRouter)
 
@@ -28,49 +20,49 @@ const routes = [
       {
         path:'/product/:id',
         name:'product',
-        component:Product
+        component:() =>import('../pages/product') 
       },
       {
         path:'/detail/:id',
         name:'detail',
-        component:Detail
+        component:() => import('../pages/detail')
       },
     ]
   },
   {
     path: '/login',
     name: 'login',
-    component: () => import('../pages/login.vue')
+    component: () => import('../pages/login')
   },
   {
     path:'/cart',
     name:'cart',
-    component:Cart
+    component:() => import('../pages/cart')
   },
   {
     path:'/order',
     name:'order',
-    component:Order,
+    component:() => import('../pages/order'),
     children:[
       {
         path:'list',
         name:'order-list',
-        component:OrderList
+        component:() => import('../pages/orderList')
       },
       {
         path:'confirm',
         name:'order-confirm',
-        component:OrderConfirm
+        component:() => import('../pages/orderConfirm')
       },
       {
         path:'pay',
         name:'order-pay',
-        component:OrderPay
+        component:() => import('../pages/orderPay')
       },
       {
         path:'alipay',
         name:'alipay',
-        component:AliPay
+        component:() => import('../pages/alipay')
       },
     ]
   }
--- vue.config.js
+++ vue.config.js
@@ -11,5 +11,18 @@ module.exports = {
         }
       }
     }
+  },
+  // 打包的资源公共路径
+  // publicPath:'/app',
+  // 打包目录
+  // outputDir:'dist',
+  // 打包单页面的入口页面名字
+  // indexPath:'index.html',
+  // 取消eslint的启用
+  // lintOnSave:false,
+  // 减少包大小且加密
+  productionSourceMap:false,
+  chainWebpack:(config)=>{
+    config.plugins.delete('prefetch');
   }
 }
